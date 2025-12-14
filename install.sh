361f9c44867299962cc62cce866a92aa8623b285
mpxss <mr.smortezapour@gmail.com>
2025-12-14 19:46:06 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "🚀 Installing Ubuntu Server Tool..."
+echo "🚀 Installing helpbox..."
 curl -fsSL https://raw.githubusercontent.com/mpxss/helpbox/main/helpbox.py -o helpbox && \
 chmod +x helpbox && \
 sudo mv toool /usr/local/bin/ && \
